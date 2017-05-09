f4accdaa85bef548999255db36bbf5949d931d02
imdasom <imdasom101@gmail.com>
2017-05-09 14:00:38 +0900

    IServiceController->ProcessCallback 이름변경

---
+++ refact/FinalSafeMeMobile/app/src/main/java/safeme/com/naver/cafe/safeme/http/ProcessCallback.java
@@ -0,0 +1,12 @@
+package safeme.com.naver.cafe.safeme.http;
+
+import android.os.Handler;
+
+/**
+ * Created by dasom on 2017-04-30.
+ */
+public interface ProcessCallback {
+    void setHandler(Handler handler);
+    void onPreExecute(Object... objects);   //파라미터를 Object...으로 하는 것은 고민해봐야 할 듯...
+    void onPostExecute(Object... objects);
+}
